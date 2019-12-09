5a304ffddb2daf253dcc0654851d876cc6543186
skas <skas@live.co.uk>
2019-12-09 17:35:28 +0300

    Basic Input.

--- Main.gd
+++ Main.gd
@@ -12,6 +12,21 @@ func _ready():
 
 func _input(event):
 	if(event.is_action_pressed("ui_left")):
+		
+		var old_left = $Left.get_child(0)
+		var old_active = $Active.get_child(0)
+		var old_right = $Right.get_child(0)
+		
+		$Left.remove_child(old_left)
+		$Active.remove_child(old_active)
+		$Right.remove_child(old_right)
+
+
+		$Right.add_child(old_active)
+		old_active.scale = $Right.scale
+		
+		$Active.add_child(old_left)
+		old_left.scale = $Active.scale
 		print("Press left")
 	pass
 #func _process(delta):
